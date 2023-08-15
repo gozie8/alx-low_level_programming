c4bd1982b19283e21bc04a07d08fcaf09eb31dd1
gozie8 <gozman2009@gmail.com>
2023-08-15 13:39:19 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_alphabet -function to print letters
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+		_putchar(letter);
+	_putchar('\n');
