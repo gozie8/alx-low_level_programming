448089333695e3cbf23a1f5421c78ca515a8f0d8
gozie8 <gozman2009@gmail.com>
2023-08-15 15:02:44 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * times_table -prints the 9 timestable
+ *
+ * Return: 9x timetable.
+ */
+void times_table(void)
+{
+	int c, d, e;
+
+
+	c = 0;
+	while (c < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			e = c * d;
+			if (e > 9)
+			{
+				_putchar(e / 10 + 48);
+				_putchar(e % 10 + 48);
+			}
+			else if (d != 0)
+			{
+				_putchar(' ');
+				_putchar(e + '0');
+			}
+			else
+				_putchar(e + 48);
+			if (d != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			++d;
+		}
+		_putchar('\n');
+		++c;
+	}
+}
