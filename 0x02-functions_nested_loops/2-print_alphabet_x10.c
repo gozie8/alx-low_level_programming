a376de17033a5a0f07a94d81ce4ca5879eeffc73
gozie8 <gozman2009@gmail.com>
2023-08-15 14:31:53 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+/* more headers goes there */
+#include "main.h"
+/**
+ * print_alphabet_x10 -function to print letters ten times
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int i = 0;
+
+while (i <= 9)
+{
+for (letter = 'a'; letter <= 'z'; ++letter)
+_putchar(letter);
+_putchar('\n');
+++i;
+}
+}
