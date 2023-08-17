e9a87a4df4dbcf9587196c1fd1609f7befc32b1e
gozie8 <gozman2009@gmail.com>
2023-08-17 10:28:51 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_numbers -prints digit
+ *
+ *Return: digits form 0 to 9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
