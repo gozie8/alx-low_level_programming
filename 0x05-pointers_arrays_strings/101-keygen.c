48f78e6d176d8f0581b0fc5572b99887b33dffae
gozie8 <gozman2009@gmail.com>
2023-08-23 12:24:14 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+
+/**
+ * min - program that generates random valid
+ * password for the program 101-crackme.
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	int pass[100];
+
+	int i, sum, n;
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass{i} = rand() % 70;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+		}
+	}
+	return (0);
+}
