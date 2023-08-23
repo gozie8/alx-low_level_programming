37ec1bdeb466fbbe49725624178c13ca2b035ba9
gozie8 <gozman2009@gmail.com>
2023-08-23 12:34:33 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 #include <time.h>
 
 /**
@@ -18,7 +18,7 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		pass{i} = rand() % 70;
+		pass{i} = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
@@ -26,6 +26,7 @@ int main(void)
 			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
+			break;
 		}
 	}
 	return (0);
