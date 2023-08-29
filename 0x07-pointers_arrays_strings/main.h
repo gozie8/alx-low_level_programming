0e0fb460a9770bba565defa56f4997c583b997cc
gozie8 <gozman2009@gmail.com>
2023-08-29 12:56:46 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
