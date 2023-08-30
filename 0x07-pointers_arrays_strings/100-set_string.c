bfc95858e29905e3e7b2dc3db626e9e14b431891
gozie8 <gozman2009@gmail.com>
2023-08-30 12:26:05 -0700

    ain.h

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,71 +1,10 @@
-/* more headers goes there */
 #include "main.h"
-/* betty style doc for function main goes there */
 /**
- * print_times_table -prints the timestable
- *@n: 1st argument
- *Return: Timetable.
+ * set_string -sets the value of a pointer to a char.
+ * @s: 1st argument
+ * @to: 2nd argument
  */
-void print_times_table(int n)
+void set_string(char **s, char *to)
 {
-	int c, d, e;
-
-
-	if (n >= 0 && n <= 15)
-	{
-		c = 0;
-		while (c <= n)
-		{
-			d = 0;
-			while (d <= n)
-			{
-				e = c * d;
-				if (e > 9)
-					digit_to_char(e);
-				else if (d != 0)
-				{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(e + '0');
-				}
-				else
-					digit_to_char(e);
-				if (d != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				++d;
-			}
-			_putchar('\n');
-			++c;
-		}
-	}
-}
-/**
- * digit_to_char -Convert numbers to character
- * @number: 1st argument
- *
- * Return: character
- */
-void digit_to_char(int number)
-{
-int x;
-
-
-if (number >= 10 && number < 100)
-{
-_putchar(' ');
-_putchar(number / 10 + 48);
-_putchar(number % 10 + 48);
-}
-else if (number >= 100)
-{
-x = number / 10;
-_putchar(x / 10 + 48);
-_putchar(x % 10 + 48);
-_putchar(number % 10 + 48);
-}
-else
-_putchar(number + 48);
+	*s = to;
 }
