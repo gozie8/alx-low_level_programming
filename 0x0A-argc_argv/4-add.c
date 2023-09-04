643bb353546c1835d9826621c8868519b55e2d4e
gozie8 <gozman2009@gmail.com>
2023-09-04 13:08:48 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -program that adds positive numbers
+ * @argc: number of program arguments
+ * @argv: array of program arguments
+ * Return: 1;
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	int i, sum = 0, num;
+	char *ch;
+
+	for (i = 1; i < argc; i++)
+	{
+		if ((*argv[i] < 48 || *argv[i] > 57))
+		num = strtol(argv[i], &ch, 10);
+		if (*argv[i] < 48 || *argv[i] > 57 || *ch != '\0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += strtol(argv[i], &ch, 10);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
