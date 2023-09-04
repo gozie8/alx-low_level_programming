c5e2ce8b8e77636fba33baf5b2ce395fa10b029b
gozie8 <gozman2009@gmail.com>
2023-09-04 12:36:24 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,19 +10,21 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc == 1){
-	}if (argc == 1 || argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			mul *= atoi(argv[i]);
-		}
-		printf("%d\n", mul);
-		return (0);
-	}
+if (argc == 1)
+{
+
+if (argc == 1 || argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+mul *= atoi(argv[i]);
+}
+printf("%d\n", mul);
+return (0);
+}
 }
