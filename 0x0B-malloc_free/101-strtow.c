bb63e2b1d47751b7fb617d4ba752271eaa90393e
gozie8 <gozman2009@gmail.com>
2023-09-05 14:52:31 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,104 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * count_word -counts the words int a sentence
+ * @str: 1st argument
+ * Return: number of words
+ */
+int count_word(char *str)
+{
+	unsigned int i = 0;
+	int flag = 0, len = 0;
+
+	while (str[i] != '\0')
+	{
+		if (flag == 1 && str[i] == ' ')
+		{
+			len++;
+			flag = 0;
+		}
+		if (str[i] != ' ')
+		{
+			flag = 1;
+		}
+		i++;
+		if (i == strlen(str) && flag == 1)
+		{
+			len++;
+		}
+	}
+	return (len);
+}
+/**
+ * count_d_char -counts the number of charcaters in a word.
+ * @m: 1st argument
+ * @str: string to be counted
+ * Return: number of charcaters in a word.
+ */
+int count_d_char(int m, char *str)
+{
+	int flag = 0, len_word = 0;
+
+	while (str[m] != '\0')
+	{
+		if (flag == 1 && str[m] == ' ')
+			break;
+		if (str[m] != ' ')
+		{
+			len_word++;
+			flag = 1;
+		}
+		m++;
+	}
+	return (len_word);
+}
+/**
+ * strtow -splits a string into words
+ * @str: 1st argument
+ * Return: a pointer to an array of strings(words)
+ */
+char **strtow(char *str)
+{
+	int j = 0, len = 0, k = 0, i;
+	char **p;
+	int flag = 0, len_word;
+
+	if (str == NULL)
+		return (NULL);
+	len  = count_word(str);
+	p = (char **) malloc((len + 1) * sizeof(char *));
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (j = 0; j < len; j++)
+	{
+		len_word = count_d_char(k, str);
+		p[j] = (char *) malloc((len_word + 1) * sizeof(char));
+		if (p[j] == NULL)
+		{
+			for (i = 0; i < j; i++)
+				free(p[i]);
+			free(p);
+			return (NULL);
+		}
+		flag = 0, i = 0;
+		while (str[k] != '\0')
+		{
+			if (flag == 1 && str[k] == ' ')
+			{
+				p[j][i] = '\0', i++;
+				break;
+			}
+			if (str[k] != ' ')
+			{
+				p[j][i] = str[k];
+				i++, flag = 1;
+			} k++;
+		}
+	}
+	p[len] = NULL;
+	return (p);
+}
